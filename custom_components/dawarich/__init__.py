b46ee559ff2a1b7f5d9876fc2742cf2d5174cce6
AlbinLind <albin.lindqvist@hotmail.se>
2024-11-03 19:05:17 +0100

    makes coordinator work with sensor entity

--- custom_components/dawarich/__init__.py
+++ custom_components/dawarich/__init__.py
@@ -7,7 +7,7 @@ from homeassistant.core import HomeAssistant
 DOMAIN = "dawarich"
 VERSION = "0.1.0"
 
-PLATFORMS: list[Platform] = [Platform.DEVICE_TRACKER]
+PLATFORMS: list[Platform] = [Platform.DEVICE_TRACKER, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
